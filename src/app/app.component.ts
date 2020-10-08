bb2452b1f2135b26239efab0902de36037fce163
Kathryn Pidek <KathrynPidek@quickenloans.com>
2020-10-08 11:28:28 -0400

    MVP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Lab163AngularToDoList';
+
+  toDoList: Todo[]= [
+    {task:"First thing", completed:false},
+    {task:"Second thing", completed:false},
+    {task:"third thing", completed:false}
+  ];
+
+  markComplete = function(i){
+     if(this.toDoList[i].completed == false)
+     {
+       this.toDoList[i].completed = true;
+     }
+     else
+     {
+      this.toDoList[i].completed = false;
+     }
+  };
+  addTask = function(){
+    
+
+    this.toDoList.push({
+      task:this.newTask,
+      completed:false
+    });
+  };
+
 }
+
+
+
+
+interface Todo {
+  task: string,
+  completed: boolean
+};
